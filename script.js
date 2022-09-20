2293622b072acb3512b803c57159c8e47cc26f2f
jfoyarzo <foyarzo621@gmail.com>
2022-09-20 18:34:20 -0300

    Fix issue with index for removing books

--- script.js
+++ script.js
@@ -17,29 +17,30 @@ window.onload = () => {
 
       removeBook = (e, newBookElement) => {
         const index = e.target.getAttribute('myIndex');
-        this.bookList = this.bookList.filter((element, i) => {
-          if (i === parseInt(index, 10)) {
-            return false;
-          }
-          return true;
-        });
+          this.bookList = this.bookList.filter((element, i) => {
+            if (i === parseInt(index, 10)) {
+              return false;
+            }
+            return true;
+          });        
         newBookElement.remove();
+        localStorage.setItem('bookArray', JSON.stringify(this.bookList));
+        this.showBooks()
       };
-
+      
       showBooks = () => {
         this.addBook.innerHTML = '';
         this.bookList.forEach((e, i) => {
           const newBook = `
           <div class="book-div">
-            <p>"${e.title}" by ${e.author}</p>
-            <button type="button" class="remove" myIndex ="${i}" >Remove</button>
+          <p>"${e.title}" by ${e.author}</p>
+          <button type="button" class="remove" myIndex ="${i}" >Remove</button>
           </div>
           `;
           const newBookElement = this.parser.parseFromString(newBook, 'text/html').body.firstChild;
           const remove = newBookElement.querySelector('.remove');
           remove.addEventListener('click', (e) => {
-            this.removeBook(e, newBookElement);
-            localStorage.setItem('bookArray', JSON.stringify(this.bookList));
+            this.removeBook(e, newBookElement);           
           });
           this.addBook.append(newBookElement);
         });
